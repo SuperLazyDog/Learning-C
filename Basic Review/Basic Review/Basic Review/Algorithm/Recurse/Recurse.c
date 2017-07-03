e0e22d98569c40cd704bf5003b18a46f0b169d52
SuperLazyDog <jyoitatsu@foxmail.com>
2017-07-03 23:59:26 +0900

    recurse now

--- Basic Review/Basic Review/Basic Review/Algorithm/Recurse/Recurse.c
+++ Basic Review/Basic Review/Basic Review/Algorithm/Recurse/Recurse.c
@@ -29,6 +29,19 @@ void recurseTest() {
     //-----------------------------------------------
     unsigned long testNum = TEST_I_RECURSE;
     printf("result: %lu\n", getFactorial(testNum));
+    
+    //-----------------------------------------------
+    //                    阶乘
+    //-----------------------------------------------
+    puts("------------------------------");
+    puts("          递归数字测试");
+    puts("------------------------------");
+    numTest_Rescurse(4);
+    
+    puts("------------------------------");
+    puts("           非递归数字测试");
+    puts("------------------------------");
+    numTest_NoRescurse(4);
 }
 
 //-------------------------------------------------------
@@ -40,4 +53,48 @@ unsigned long getFactorial(unsigned long num) {
         return 1;
     }
     return num * getFactorial(num-1);
+}
+
+//-------------------------------------------------------
+//                      表示数字
+//-------------------------------------------------------
+//递归法
+void numTest_Rescurse(int num) {
+    if (num > 0) {
+        numTest_Rescurse(num-1);
+        printf("num: %d\n", num);
+        numTest_Rescurse(num-2);
+    }
+}
+//去除递归
+void numTest_NoRescurse(int num) {
+    Stack stack;
+    Capacity max = 100;
+    initialize_Stack(&stack, max);
+Sample:
+    if (num > 0) {
+        push_In_Stack(&stack, (Stack_Data *)&num);
+        num = num - 1;
+        goto Sample;
+    }
+    if (isEmpty_Stack(&stack) == false) {
+        pop_From_Stack(&stack, &num);
+        printf("num: %d\n", num);
+        num = num - 2;
+        goto Sample;
+    }
+    terminate_Stack(&stack);
+/*Sample:
+    if (num > 0) {
+        numTest_NoRescurse(num-1);
+        printf("num: %d\n", num);
+        num = num - 2;
+        goto Sample;
+    }*/
+    
+    /*while (num > 0) {
+        numTest_NoRescurse(num-1);
+        printf("num: %d\n", num);
+        num = num - 2;
+    }*/
 }
