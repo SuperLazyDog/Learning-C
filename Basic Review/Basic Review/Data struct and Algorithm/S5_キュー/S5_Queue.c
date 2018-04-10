d7d2cec9fb6d0052f97a663a6c05ae35e235337e
SuperLazyDog <jyoitatsu@foxmail.com>
2018-04-11 01:21:53 +0900

    Temp: [队列: 静态数组法]

--- Basic Review/Basic Review/Data struct and Algorithm/S5_キュー/S5_Queue.c
+++ Basic Review/Basic Review/Data struct and Algorithm/S5_キュー/S5_Queue.c
@@ -12,8 +12,58 @@
 //                            序列 p111~122
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
-//                       4.5.1 单纯数组实现 p83
+//                       5.6.2 单纯循环数组实现 p113
 //---------------------------------------------------------------------
+//------------------------------------------------
+//                  序列的型声明
+//------------------------------------------------
+struct ArrayQueue {
+	int front, rear;
+	int capacity;
+	int *array;
+};
+
+//------------------------------------------------
+//                   函数声明
+//------------------------------------------------
+// SHOW: SAMPLE [书上版本]
+struct ArrayQueue *Queue(int size); // 初始化
+int isEmptyQueue(struct ArrayQueue *Q); // 是否为空
+int isFullQueue(struct ArrayQueue *Q); // 是否已满
+int QueueSize(struct ArrayQueue *Q); // 获取大小
+int enQueue(struct ArrayQueue *Q, int data); // push元素
+int deQueue(struct ArrayQueue *Q); // unshift元素
+int deleteQueue(struct ArrayQueue *Q); // 删除整个队列
+//------------------------------------------------
+//                   函数实现
+//------------------------------------------------
+struct ArrayQueue *Queue(int size) { // 初始化
+	return NULL;
+}
+
+int isEmptyQueue(struct ArrayQueue *Q) { // 是否为空
+	return NULL;
+}
+
+int isFullQueue(struct ArrayQueue *Q) { // 是否已满
+	return NULL;
+}
+
+int QueueSize(struct ArrayQueue *Q) { // 获取大小
+	return NULL;
+}
+
+int enQueue(struct ArrayQueue *Q, int data) { // push元素
+	return NULL;
+}
+
+int deQueue(struct ArrayQueue *Q) { // unshift元素
+	return NULL;
+}
+
+int deleteQueue(struct ArrayQueue *Q) { // 删除整个队列
+	return NULL;
+}
 
 //---------------------------------------------------------------------
 //                             测试函数
